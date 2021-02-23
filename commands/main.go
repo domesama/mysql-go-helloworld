b87afd93d1adc0eb97b324b7ad30bf599d63e0ae
Thethawat Kongyu <thethawat.k@mail.kmutt.ac.th>
2021-02-23 16:13:55 +0700

    [Add] API with implemented services

--- commands/main.go
+++ commands/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/domesama/mysql-go-helloworld/platform/newsfeed"
+	"github.com/go-chi/chi"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/qkgo/yin"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -16,11 +18,37 @@ func main() {
 	}
 
 	feed := newsfeed.NewFeed(db)
-
+	//
 	//feed.AddFeed(newsfeed.Item{Content: "UwU"})
-	items := feed.GetFeed()
+	//items := feed.GetFeed()
+	//
+	//fmt.Println(items)
 
-	fmt.Println(items)
+	r := chi.NewRouter()
+	r.Use(yin.SimpleLogger)
+
+	r.Get("/posts",func(w http.ResponseWriter, r *http.Request){
+		res, _ := yin.Event(w,r)
+		items := feed.GetFeed()
+
+		res.SendJSON(items)
+	})
+
+	r.Post("/posts", func(w http.ResponseWriter, r *http.Request){
+		res, req := yin.Event(w,r)
+		body := map[string]string{}
+		req.BindBody(&body)
+
+		item := newsfeed.Item{
+			Content: body["content"],
+		}
+
+		feed.AddFeed(item)
+
+		res.SendStatus(200)
+	})
+	
+	http.ListenAndServe(":8080", r)
 
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/domesama/mysql-go-helloworld
 
 go 1.15
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/go-chi/chi v1.5.3
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/qkgo/yin v0.0.0-20190403073632-bff3b50ba6e6
+)
